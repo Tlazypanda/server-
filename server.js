9659f831198719f3a068348d5ea3287cb76d3166
Tlazypanda <33183263+Tlazypanda@users.noreply.github.com>
2017-12-13 18:32:36 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
+
+var mimeTypes = {
+    "html" : "text/html",
+    "jpeg" : "image/jpeg",
+    "jpg" : "image/jpeg",
+    "png" : "image/png",
+    "js" : "text/javascript",
+    "css" : "text/css"
+};
+
+http.createServer(function (req, res) {
+var uri = url.parse(req.url).pathname;
+var fileName = path.join(process.cwd(), unescape(uri));
+console.log('Loading' + uri);
+    var stats;
+    
+
+                  
+try {
+      stats = fs.lstatSync(fileName) ;           
+                  
+                  } catch(e) {
+    res.writeHead(404,{'Content-type': 'text/plain'});
+    res.write('404 Not Found\n');
+    res.end();
+    return;
+}  
+                  
+ if(stats.isFile()){
+     var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+     res.writeHead(200, {'Content-type': mimeType});
+     var fileStream = fs.createReadStream(fileName);
+     fileStream.pipe(res);
+ }
+                  
+                  
+                  }).listen(3001);
